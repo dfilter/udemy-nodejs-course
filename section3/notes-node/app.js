dac51c8208cda3254b95a5a452bb38b36a57cb0c
dfilter <dirk.b.filter@hotmail.ca>
2018-07-03 23:50:44 -0400

    changes thus far

---
+++ section3/notes-node/app.js
@@ -0,0 +1,40 @@
+console.log('Starting app.js')
+
+/**
+ * Lesson 1:
+ * To find moduals and other api for node js see https://nodehs.org/api
+ * "require" is used to load a modual. fs can now be used to access all the functions 
+ * that are part of this module.
+ * "appendFile()" is used for simple file io. Here we write to a file using the fs 
+ * imported earler from the fs modual.
+ * Note: the latest version of nodejs requires function callback when using appendFile.
+ * Optionally if you want to do write to the file syncronously you can use the 
+ * "appendFileSync()" function.
+ * The second parameter passed to appendFile() below is some ES6 syntax that allows for
+ * easy concatination of strings and variables.
+ * 
+ * Lesson 2:
+ * "./" inside of the require parameter is a reletive path meaning it points to the current 
+ * directory.
+ * 
+ */
+
+const fs = require('fs')
+const os = require('os')
+const notes = require('./notes.js')
+
+var res = notes.addNote()
+console.log(res)
+var sum = notes.addNumbers(1, 2)
+console.log(sum)
+// var user = os.userInfo()
+
+// // Note: the following is depreciated...
+// //fs.appendFile('greetings.txt', 'Hello World!')
+// // use one of the following instead:
+// //fs.appendFileSync('greetings.txt', 'Hello World!')
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, error => {
+//     if (error) {
+//         console.log('Unable to write to file.')
+//     }
+// })
---
+++ section3/notes-node/notes.js
@@ -0,0 +1,14 @@
+console.log('Starting notes.js')
+
+/**
+ * 
+ */
+module.exports.addNote = () => {
+    console.log('addNote')
+    return 'New note'
+}
+
+module.exports.addNumbers = (number1, number2) => {
+    console.log('addNumbers')
+    return number1 + number2
+}
