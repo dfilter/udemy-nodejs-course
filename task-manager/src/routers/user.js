85f39a4433655670da7b9a217db2a36b53fb96ff
dfilter <dfilter@evertz.com>
2020-01-30 17:10:03 -0500

    111

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -26,6 +26,26 @@ router.post('/users/login', async (req, res) => {
   }
 })
 
+router.post('/users/logout', auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter(token => token.token !== req.token)
+    await req.user.save()
+    res.send()
+  } catch (error) {
+    res.status(500).send()
+  }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+  try {
+    req.user.tokens = []
+    await req.user.save()
+    res.send()
+  } catch (error) {
+    res.status(500).send()
+  }
+})
+
 // Here we add middleware as second arg. It will run before handling function callback
 router.get('/users/me', auth, async (req, res) => {
   res.send(req.user)
