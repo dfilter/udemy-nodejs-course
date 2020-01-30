109132d0df01530a56927dba9e8fbabcc317a58a
dfilter <dfilter@evertz.com>
2020-01-30 14:56:31 -0500

    105

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,7 +1,8 @@
+const bcrypt = require('bcryptjs')
 const mongoose = require('mongoose')
 const validator = require('validator')
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
@@ -9,6 +10,7 @@ const User = mongoose.model('User', {
   },
   email: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
     lowercase: true,
@@ -39,5 +41,28 @@ const User = mongoose.model('User', {
     }
   }
 })
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email })
+  if (!user) {
+    throw new Error('Unable to login')
+  }
+  const isMatch = await bcrypt.compare(password, user.password)
+  if (!isMatch) {
+    throw new Error('Unable to login')
+  }
+  return user
+}
+
+// Note that a regular function is used here, this is because "this" needs to be accessible inside function scope
+userSchema.pre('save', async function (next) {
+  let user = this
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8)
+  }
+  next()  // ends function's runtime this is required
+})
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
