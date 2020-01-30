cc33122b10bd240111a14d7210d6870cff6a6c85
dfilter <dfilter@evertz.com>
2020-01-30 16:09:19 -0500

    108

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -10,6 +10,20 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
+// Middleware must be set before other app.use() calls
+// app.use((req, res, next) => {
+//   console.log(req.method, req.path)
+//   if (req.method == 'GET') {
+//     res.send('GET requests are disabled')
+//   } else {
+//     next()  // must call next to end function
+//   }
+// })
+
+app.use((req, res, next) => {
+  res.status(503).send('Site is currently down check back soon!')
+})
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
