2eb4c39b34fbc5e79757040d0a0f68e49f56318e
dfilter <dfilter@evertz.com>
2020-08-06 12:18:22 -0400

    139. Writing Your Own Tests

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,4 +1,4 @@
-const { calculateTip } = require('../src/math')
+const { calculateTip, celsiusToFahrenheit, fahrenheitToCelsius } = require('../src/math')
 
 test('Should calculate total with tip', () => {
   const total = calculateTip(10, .3)
@@ -8,4 +8,14 @@ test('Should calculate total with tip', () => {
 test('Should calculate total with default tip', () => {
   const total = calculateTip(10)
   expect(total).toBe(12.5)
+})
+
+test('Should calculate conversion of Fahrenheit (32) to Celsius (0)', () => {
+  const temperature = fahrenheitToCelsius(32)
+  expect(temperature).toBe(0)
+})
+
+test('Should calculate conversion of Celsius (0) to Fahrenheit (32)', () => {
+  const temperature = celsiusToFahrenheit(0)
+  expect(temperature).toBe(32)
 })
