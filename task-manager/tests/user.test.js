3d42f9f1b3f7bcf6645d96b0356a6b3fbda84323
dfilter <dfilter@evertz.com>
2020-08-07 11:12:09 -0400

    144. Testing with Authentication

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,11 +1,78 @@
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 const request = require('supertest')
 
 const app = require('../src/app')
+const User = require('../src/models/user')
+
+const userOneId = new mongoose.Types.ObjectId()
+const userOne = {
+  _id: userOneId,
+  name: 'John',
+  email: 'john@email.com',
+  password: 'johnpass123!',
+  tokens: [{
+    token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
+  }]
+}
+
+beforeEach(async () => {
+  await User.deleteMany()
+  await new User(userOne).save()
+})
 
 test('Should signup a new user', async () => {
   await request(app).post('/users').send({
-    name: 'John',
-    email: 'johndoe@email.com',
-    password: 'MyPassword123!'
+    name: 'Test',
+    email: 'test@gmail.com',
+    password: 'testing123!'
   }).expect(201)
+})
+
+test('Should login existing user', async () => {
+  await request(app).post('/users/login').send({
+    email: userOne.email,
+    password: userOne.password
+  }).expect(200)
+})
+
+test('Should reject login of nonexisting user', async () => {
+  await request(app).post('/users/login').send({
+    email: 'jane@email.com',
+    password: 'janepass123!'
+  }).expect(400)
+})
+
+test('Should get profile for user', async () => {
+  await request(app)
+    .get('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200)
+})
+
+test('Should fail to get profile for unauthorized user', async () => {
+  await request(app)
+    .get('/users/me')
+    .send()
+    .expect(401)
+})
+
+test('Should delete profile for user', async () => {
+  await request(app)
+    .delete('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200)
+})
+
+test('Should fail to delete profile for unauthorized user', async () => {
+  await request(app)
+    .delete('/users/me')
+    .send()
+    .expect(401)
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
 })
