b1e3673b27a82d297af54249aa262cab13ca24b1
dfilter <dfilter@evertz.com>
2020-08-10 17:29:50 -0400

    168. Storing Users: Part I

---
+++ chat-app/src/utils/users.js
@@ -0,0 +1,49 @@
+const users = []
+
+const addUser = ({ id, username, room }) => {
+  // clean data
+  username = username.trim().toLowerCase()
+  room = room.trim().toLowerCase()
+
+  // validate data
+  if (!username || !room) {
+    return {
+      error: 'Username and room are required!'
+    }
+  }
+
+  // check for existing user
+  const existingUser = users.find((user) => {
+    return user.room === room && user.username == username
+  })
+
+  // validate username
+  if (existingUser) {
+    return {
+      error: 'Username is in use!'
+    }
+  }
+
+  // store user
+  const user = { id, username, room }
+  users.push(user)
+  return { user }
+}
+
+
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id)
+  if (index !== -1) {
+    return users.splice(index, 1)[0]
+  }
+}
+
+addUser({
+  id: 22,
+  username: 'jdoe',
+  room: 'room1'
+})
+console.log(users)
+const removedUser = removeUser(22)
+console.log(removedUser)
+console.log(users)
