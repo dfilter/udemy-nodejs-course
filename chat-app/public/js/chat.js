8cf5db695aafc12a4fa1f593045c913808633e38
dfilter <dfilter@evertz.com>
2020-08-10 14:07:52 -0400

    156. Socket.io Events Challenge

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,10 +1,14 @@
 const socket = io()
 
-socket.on('countUpdated', (count) => {
-  console.log(`The count has been updated: ${count}`)
+socket.on('message', (message) => {
+  console.log(message)
 })
 
-document.querySelector('#increment').addEventListener('click', () => {
-  console.log('Clicked!')
-  socket.emit('increment')
+const messageForm = document.querySelector('#message-form')
+
+messageForm.addEventListener('submit', (event) => {
+  event.preventDefault()
+  
+  socket.emit('sendMessage', event.target.elements.message.value)
+  event.target.elements.message.value = null
 })
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -13,19 +13,12 @@ const io = socketio(server)
 
 app.use(express.static(publicDirectoryPath))
 
-let count = 0
-
 io.on('connection', (socket) => {
   console.log('New Websocket Connection.')
+  socket.emit('message', 'Welcome!')
 
-  // server emits increment event to clients
-  socket.emit('countUpdated', count)
-
-  // server receives increment event from client
-  socket.on('increment', () => {
-    count++
-    // socket.emit('countUpdated', count)  // only send to this connection
-    io.emit('countUpdated', count)  // emit count to all connected client
+  socket.on('sendMessage', (message) => {
+    io.emit('message', message)
   })
 })
 
