73263c28b25d7b0f4dc8c58054bb087e4a53fcde
dfilter <dfilter@evertz.com>
2020-08-10 18:12:17 -0400

    171. Sending Messages to Rooms

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -26,29 +26,31 @@ io.on('connection', (socket) => {
       return callback(error)
     }
     socket.join(user.room)
-    socket.emit('message', generateMessage('Welcome!'))
-    socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined.`))  // broadcast only to the joined room
+    socket.emit('message', generateMessage('Chat App', 'Welcome!'))
+    socket.broadcast.to(user.room).emit('message', generateMessage(user.username, `${user.username} has joined.`))  // broadcast only to the joined room
     callback()
   })
 
   socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id)
     const filter = new Filter()
     if (filter.isProfane(message)) {
       return callback('Profanity is not allowed!')
     }
-    io.to('room1').emit('message', generateMessage(message))
+    io.to(user.room).emit('message', generateMessage(user.username, message))
     callback()
   })
 
   socket.on('sendLocation', (coords, callback) => {
-    io.emit('locationMessage', generateLocationMessage(coords))
+    const user = getUser(socket.id)
+    io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, coords))
     callback()
   })
 
   socket.on('disconnect', () => {
     const user = removeUser(socket.id)
     if (user) {
-      io.to(user.room).emit('message', generateMessage(`${user.username} has left.`))
+      io.to(user.room).emit('message', generateMessage(user.username, `${user.username} has left.`))
     }
   })
 })
