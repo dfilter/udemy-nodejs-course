d425a0bfc0bfe092f9f4df83c74b938f1a0fb2c3
dfilter <dfilter@evertz.com>
2020-08-10 14:42:10 -0400

    158. Sharing Your Location

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -5,10 +5,24 @@ socket.on('message', (message) => {
 })
 
 const messageForm = document.querySelector('#message-form')
+const messageBox = document.querySelector('#message-box')
+const locationButton = document.querySelector('#send-location')
 
 messageForm.addEventListener('submit', (event) => {
   event.preventDefault()
-  
-  socket.emit('sendMessage', event.target.elements.message.value)
-  event.target.elements.message.value = null
+
+  socket.emit('sendMessage', messageBox.value)
+  messageBox.value = null
+})
+
+locationButton.addEventListener('click', (event) => {
+  if (!navigator.geolocation) {
+    return alert('Geolocation is not supported by your browser.')
+  }
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit('sendLocation', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    })
+  })
 })
